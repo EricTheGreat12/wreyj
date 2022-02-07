4b512300446e2e0f8ae08517501941682d1bc279
Eric <culver.eric.s@gmail.com>
2022-02-07 18:24:37 -0500

    basic gameboard styling done

--- src/main.js
+++ src/main.js
@@ -12,15 +12,27 @@ const page = (() => {
         const gameBoard = document.createElement("div");
         gameBoard.id = "gameBoard";
         let row;
+        let squareContainer;
         let square;
         for (let i = 0; i < 6; i++) {
             row = document.createElement("div");
             row.classList.add("gameBoardRow");
             for (let x = 0; x < 5; x++) {
+                squareContainer = document.createElement("div");
+                squareContainer.classList.add("squareContainer");
+                row.append(squareContainer);
+
                 square = document.createElement("div");
                 square.textContent = "W";
-                square.classList.add("gameBoardSquare");
-                row.append(square);
+                square.classList.add("square");
+                squareContainer.append(square);
+
+                if (i == 1) {
+                    square.classList.add("present");
+                }
+                if (i == 3) {
+                    square.classList.add("correct");
+                }
             }
             gameBoard.append(row);
         }
