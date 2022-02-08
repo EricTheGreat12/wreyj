1e357732d89f28dc8e7915ab938e890faff100ee
eric <culver.eric.s@gmail.com>
2022-02-08 16:45:53 -0500

    made keyboard.js an IIFE

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,47 +1,49 @@
-const chars = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+export const keyboard = (() => {
+
+    const chars = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
         ['spacer', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'spacer'],
         ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']];
 
-const makeKeyboard = () => {
-    const keyBoard = document.createElement("div");
-    keyBoard.id = "keyboard";
-    for (let i in chars) {
-        let row = document.createElement("div");
-        row.classList.add("keyboardRow");
-        keyBoard.append(row);
-        for (let x in chars[i]) {
-            row.append(makeKey(chars[i][x]));
+    const make = () => {
+        const keyBoardDiv = document.createElement("div");
+        keyBoardDiv.id = "keyboard";
+        for (let i in chars) {
+            let row = document.createElement("div");
+            row.classList.add("keyboardRow");
+            keyBoardDiv.append(row);
+            for (let x in chars[i]) {
+                row.append(makeKey(chars[i][x]));
+            }
         }
+        return keyBoardDiv;
     }
-    return keyBoard;
-}
 
-const makeKey = (char) => {
-    let key;
-    if (char == 'spacer') {
-        key = document.createElement("div");
-        key.classList.add(char);
+    const makeKey = (char) => {
+        let key;
+        if (char == 'spacer') {
+            key = document.createElement("div");
+            key.classList.add(char);
+        }
+        else if (char == 'ENTER') {
+            key = document.createElement("button");
+            key.id = char;
+            key.textContent = char;
+            key.classList.add("key", "bigKey");
+        }
+        else if (char == 'DEL') {
+            key = document.createElement("button");
+            key.id = char;
+            key.textContent = char;
+            key.classList.add("key", "bigKey");
+        }
+        else {
+            key = document.createElement("button");
+            key.id = char;
+            key.textContent = char;
+            key.classList.add("key");
+        }
+        
+        return key;
     }
-    else if (char == 'ENTER') {
-        key = document.createElement("button");
-        key.id = char;
-        key.textContent = char;
-        key.classList.add("key", "bigKey");
-    }
-    else if (char == 'DEL') {
-        key = document.createElement("button");
-        key.id = char;
-        key.textContent = char;
-        key.classList.add("key", "bigKey");
-    }
-    else {
-        key = document.createElement("button");
-        key.id = char;
-        key.textContent = char;
-        key.classList.add("key");
-    }
-    
-    return key;
-}
-
-export { makeKeyboard }
+    return { make }
+})();
--- src/main.js
+++ src/main.js
@@ -1,56 +1,11 @@
 import { gameBoard } from "./gameBoard";
 import { makeHeader } from "./header";
+import { keyboard } from "./keyboard";
 import "./style.css"
 const page = (() => {
-    const chars = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-            ['spacer', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'spacer'],
-            ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']];
-
-    const makeKeyboard = () => {
-        const keyBoard = document.createElement("div");
-        keyBoard.id = "keyboard";
-        for (let i in chars) {
-            let row = document.createElement("div");
-            row.classList.add("keyboardRow");
-            keyBoard.append(row);
-            for (let x in chars[i]) {
-                row.append(makeKey(chars[i][x]));
-            }
-        }
-        return keyBoard;
-    }
-
-    const makeKey = (char) => {
-        let key;
-        if (char == 'spacer') {
-            key = document.createElement("div");
-            key.classList.add(char);
-        }
-        else if (char == 'ENTER') {
-            key = document.createElement("button");
-            key.id = char;
-            key.textContent = char;
-            key.classList.add("key", "bigKey");
-        }
-        else if (char == 'DEL') {
-            key = document.createElement("button");
-            key.id = char;
-            key.textContent = char;
-            key.classList.add("key", "bigKey");
-        }
-        else {
-            key = document.createElement("button");
-            key.id = char;
-            key.textContent = char;
-            key.classList.add("key");
-        }
-        
-        return key;
-    }
-
     const make = () => {
         document.body.append(
-            makeHeader(), gameBoard.make(), makeKeyboard());
+            makeHeader(), gameBoard.make(), keyboard.make());
     }
 
     const reset = () => {
@@ -60,7 +15,7 @@ const page = (() => {
         make();
     }
 
-    return { make, reset }
+    return { reset }
 })();
 
-page.make();
+page.reset();
